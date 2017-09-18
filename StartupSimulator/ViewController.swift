f18d5c373ea75d71bc6d4245ec6c4c3f405f6a31
pbokey <pbokey@gmail.com>
2017-09-18 14:03:22 -0400

    Initial Commit

---
+++ StartupSimulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartupSimulator
+//
+//  Created by Pranav Bokey on 9/18/17.
+//  Copyright © 2017 Pranav Bokey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
